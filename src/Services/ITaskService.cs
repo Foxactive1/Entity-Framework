7324122f6dd8f3131ad24b89e5fbc91c2f7b53d5
Dione Castro Alves <84285490+Foxactive1@users.noreply.github.com>
2025-11-24 19:57:06 -0300

    Define ITaskService interface for task operations

--- src/Services/ITaskService.cs
+++ src/Services/ITaskService.cs
@@ -1,1 +1,18 @@
+using InnovaTasks.Api.DTOs;
+using InnovaTasks.Api.Models;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
+namespace InnovaTasks.Api.Services
+{
+    public interface ITaskService
+    {
+        Task<IEnumerable<TaskReadDto>> GetAllAsync();
+        Task<TaskReadDto?> GetByIdAsync(Guid id);
+        Task<TaskReadDto> CreateAsync(TaskCreateDto dto);
+        Task<bool> UpdateAsync(Guid id, TaskCreateDto dto);
+        Task<bool> SetCompletedAsync(Guid id, bool completed);
+        Task<bool> DeleteAsync(Guid id);
+    }
+}
