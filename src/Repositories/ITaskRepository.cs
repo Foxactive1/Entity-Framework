b53f65c3ed103bf17ed5498a6a999026f449fc36
Dione Castro Alves <84285490+Foxactive1@users.noreply.github.com>
2025-11-24 19:55:23 -0300

    Define ITaskRepository interface for task management

--- src/Repositories/ITaskRepository.cs
+++ src/Repositories/ITaskRepository.cs
@@ -1,1 +1,17 @@
+using InnovaTasks.Api.Models;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
+namespace InnovaTasks.Api.Repositories
+{
+    public interface ITaskRepository
+    {
+        Task<IEnumerable<TaskItem>> GetAllAsync();
+        Task<TaskItem?> GetByIdAsync(Guid id);
+        Task AddAsync(TaskItem item);
+        Task UpdateAsync(TaskItem item);
+        Task DeleteAsync(Guid id);
+        Task SaveChangesAsync();
+    }
+}
